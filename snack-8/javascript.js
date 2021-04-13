5b89bdf467c8f15a66f304b0420dace28beb3f6e
malesani <alejandroMalesani@hotmail.com>
2021-04-13 17:24:18 +0200

    snack-8 part 2

--- snack-8/javascript.js
+++ snack-8/javascript.js
@@ -5,10 +5,10 @@ var valor = prompt ("digita un numero di 4 cifre")
 //somma di quel numero 
 
 var numero = valor;
-    cadenaNumero = numero.toString();
-    suma=0;
-for (var i = 0; i < cadenaNumero.length; i += 1) {
-    suma+=(+cadenaNumero.charAt(i));
+    cadenaNumero = numero;
+     var suma = 0;
+for (var i = 0; i < cadenaNumero.length; i++) {
+    suma+=(parseInt(cadenaNumero.charAt(i)));
 }
 
 console.log(suma);

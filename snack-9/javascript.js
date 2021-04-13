fc7d4e59bf2bd4b95423b1c3cbcbc54cb66a699e
malesani <alejandroMalesani@hotmail.com>
2021-04-13 17:52:24 +0200

    snack - 9 finish

---
+++ snack-9/javascript.js
@@ -0,0 +1,13 @@
+
+var risultato_sum = 0;
+
+for( var i = 0; i <=10; i++){
+ var numeri = (i)
+ risultato_sum = risultato_sum + numeri;
+}
+
+console.log(risultato_sum);
+
+var media = risultato_sum / 10; 
+console.log(media);
+
